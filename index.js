0e3abb2a8a96f99bb945e6b38ff0f15a2dc3cff0
shivammeh <31971798+shivammeh@users.noreply.github.com>
2022-05-15 21:55:19 -0500

    Update index.js
    test commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiK
 
 request(url, function (err, response, body) {
     if(err){
-      console.log('error:', error);
+      console.log('error:',   error);
     } else {
       let weather = JSON.parse(body);
       let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
